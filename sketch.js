1a7422eceb920bfd0b8a900ac190e8ce0a8e30d5
Ayushi Shah <as17726@nyu.edu>
2023-09-13 15:24:20 -0400

    two ellipses and one square

--- sketch.js
+++ sketch.js
@@ -1,5 +1,16 @@
 function setup() {
+    createCanvas(600, 600); 
+    background (153,124,167);
+
 }
 
 function draw() {
+fill ("green");
+  ellipse(100, 100, 100, 100);
+  fill("orange");
+  ellipse(200, 200, 200, 200);
+  fill("blue");
+  stroke(255, 0, 50);
+  strokeweight(5);
+  rect(50, 50, 10, 10);
 }
