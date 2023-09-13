0159998c5a80c1f05bea8e0c85b49e7c65f53045
Ayushi Shah <as17726@nyu.edu>
2023-09-13 16:20:32 -0400

    Bear

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+let eyeWidth = 100; 
+let facewidth = 300; 
 function setup() {
     createCanvas(600, 600); 
     background (153,124,167);
@@ -5,12 +7,22 @@ function setup() {
 }
 
 function draw() {
-fill ("green");
-  ellipse(100, 100, 100, 100);
-  fill("orange");
-  ellipse(200, 200, 200, 200);
-  fill("blue");
-  stroke(255, 0, 50);
-  strokeweight(5);
-  rect(50, 50, 10, 10);
+    strokeWeight(3);
+    stroke(0); 
+fill ("orange");
+  ellipse(100, 300,eyeWidth,eyeWidth);
+  ellipse(300, 300,eyeWidth,eyeWidth);
+  fill ("orange")
+  ellipse (200,400,facewidth,facewidth);
+  strokeWeight(2);
+fill(255);
+  arc(150, 350, 50, 80, HALF_PI + QUARTER_PI,QUARTER_PI,CHORD);
+  arc(250, 350, 50, 80, HALF_PI + QUARTER_PI,QUARTER_PI,CHORD);
+fill(0)
+arc(150, 360, 25, 40, HALF_PI + QUARTER_PI,QUARTER_PI,CHORD);
+arc(250, 360, 25, 40, HALF_PI + QUARTER_PI,QUARTER_PI,CHORD);
+ellipse(200,400,30,30);
+line(200,400,200,450);
+fill("red");
+triangle(150, 450,250,450,200,500);
 }
